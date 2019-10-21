f9e4ee83ce0b1285b929bdd970b73c66d0f62d40
Dextrous <ssjdexucyche@gmail.com>
2019-10-21 18:11:16 +0530

    initial commit - no components

---
+++ src/Components/Pallette/Pallette.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Pallette extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Pallette</h1>
+      </div>
+    );
+  }
+}
