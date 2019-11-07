4609085dde9f314106d38f28afc47b4652653a01
Dextrous <ssjdexucyche@gmail.com>
2019-11-07 16:49:35 +0530

    Updates

---
+++ precache-manifest.8f57afeab11b5f844d4904d43a627846.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17bfa7e04e78cbc1dbc896e16b03695d",
+    "url": "/colorsprojectreact/index.html"
+  },
+  {
+    "revision": "ba14fecd8211843fe754",
+    "url": "/colorsprojectreact/static/css/2.292c8731.chunk.css"
+  },
+  {
+    "revision": "5d889ba744a7373d3a84",
+    "url": "/colorsprojectreact/static/css/main.7cc72dce.chunk.css"
+  },
+  {
+    "revision": "ba14fecd8211843fe754",
+    "url": "/colorsprojectreact/static/js/2.60bb5187.chunk.js"
+  },
+  {
+    "revision": "5d889ba744a7373d3a84",
+    "url": "/colorsprojectreact/static/js/main.e78b788f.chunk.js"
+  },
+  {
+    "revision": "132f501f278374f0e547",
+    "url": "/colorsprojectreact/static/js/runtime-main.5865a5a1.js"
+  },
+  {
+    "revision": "f9562440fbc7da5b99d5f7b4223db155",
+    "url": "/colorsprojectreact/static/media/bg.f9562440.svg"
+  }
+]);
